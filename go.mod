c77079312038de4b7dc959d90a4569fa2e93cfc5
abhijith shenoy <abhijith2831@gmail.com>
2024-09-18 03:19:14 +0530

    Add authentication and HTML templating

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/shenoyabhijith/bookstore-api
 go 1.23.1
 
 require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/gorilla/sessions v1.4.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.7.1 // indirect
--- main.go
+++ main.go
@@ -1,53 +1,78 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
-	"net/http"
+    "html/template"
+    "log"
+    "net/http"
 
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+    "github.com/gorilla/mux"
+    "github.com/gorilla/sessions"
+    "gorm.io/driver/postgres"
+    "gorm.io/gorm"
 )
 
-type Book struct {
-	ID     uint   `json:"id" gorm:"primaryKey"`
-	Title  string `json:"title"`
-	Author string `json:"author"`
+var (
+    db    *gorm.DB
+    err   error
+    store = sessions.NewCookieStore([]byte("super-secret-key"))
+    tpl   *template.Template
+)
+
+func init() {
+    tpl = template.Must(template.ParseGlob("templates/*.html"))
 }
 
-var db *gorm.DB
-var err error
+type Book struct {
+    ID     uint   `json:"id" gorm:"primaryKey"`
+    Title  string `json:"title"`
+    Author string `json:"author"`
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method == http.MethodPost {
+        username := r.FormValue("username")
+        password := r.FormValue("password")
+
+        // Simple authentication logic
+        if username == "admin" && password == "password" {
+            session, _ := store.Get(r, "session")
+            session.Values["authenticated"] = true
+            session.Save(r, w)
+            http.Redirect(w, r, "/books", http.StatusSeeOther)
+            return
+        }
+        http.Error(w, "Forbidden", http.StatusForbidden)
+        return
+    }
+    tpl.ExecuteTemplate(w, "login.html", nil)
+}
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
-	var books []Book
-	db.Find(&books)
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(books)
+    session, _ := store.Get(r, "session")
+    if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+        http.Redirect(w, r, "/login", http.StatusFound)
+        return
+    }
+
+    var books []Book
+    db.Find(&books)
+    tpl.ExecuteTemplate(w, "books.html", books)
 }
 
 func main() {
-	// Use port 5432 if port-forwarding, or 30007 if using NodePort
-	dsn := "host=localhost user=postgres password=mysecretpassword dbname=postgres port=5432 sslmode=disable"
-	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Failed to connect to database:", err)
-	}
+    dsn := "host=localhost user=postgres password=mysecretpassword dbname=postgres port=5432 sslmode=disable"
+    db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    if err != nil {
+        log.Fatal("Failed to connect to database:", err)
+    }
 
-	db.AutoMigrate(&Book{})
+    db.AutoMigrate(&Book{})
 
-	// Seed data if table is empty
-	var count int64
-	db.Model(&Book{}).Count(&count)
-	if count == 0 {
-		books := []Book{
-			{Title: "The Go Programming Language", Author: "Alan A. A. Donovan"},
-			{Title: "Introducing Go", Author: "Caleb Doxsey"},
-			{Title: "Go in Action", Author: "William Kennedy"},
-		}
-		db.Create(&books)
-	}
+    router := mux.NewRouter()
+    router.HandleFunc("/login", loginHandler)
+    router.HandleFunc("/books", getBooks).Methods("GET")
 
-	http.HandleFunc("/books", getBooks)
-	log.Println("Server started at :8081")
-	log.Fatal(http.ListenAndServe(":8081", nil))
+    log.Println("Server started at :8081")
+    log.Fatal(http.ListenAndServe(":8081", router))
 }
+ 
